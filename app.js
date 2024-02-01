b2966ea2b40a08e37a51785b14d67de5d269957f
Emanuel Henkel Gonçalves <emanuel.henkel@gmail.com>
2024-01-31 21:59:27 -0300

    Instalação do cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let cors = require("cors")
 
 require("dotenv").config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use(indexRouter);
 
